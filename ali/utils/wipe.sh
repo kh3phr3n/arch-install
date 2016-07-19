87e35a79fc70d9251c9c242aad6d238d71a187c6
kh3phr3n <rc.dev@tuxico.com>
2016-07-19 10:53:25 +0200

    Update wipe.sh (Add ATA Secure Erase for SSD)

--- ali/utils/wipe.sh
+++ ali/utils/wipe.sh
@@ -19,22 +19,30 @@ YELLOW='\e[1;33m'
 HARDDISK='/dev/sda'
 # Hard Disk Drive size (B)
 HARDDISKSIZE=$(blockdev --getsize64 ${HARDDISK})
+# Hard Disk Drive type (0 = SSD)
+HARDDISKTYPE=$(cat /sys/block/${HARDDISK##*/}/queue/rotational)
 
 # Zero-fill
 echo -e "${BLUE}:: Zero-fill device: ${CYAN}${HARDDISK}\n${OFF}"
 dd if=/dev/zero of=${HARDDISK} bs=4096 count=${HARDDISKSIZE} iflag=count_bytes status=progress && echo -e "${YELLOW}\n:: Press any key to continue...${OFF}"; read
 
+# Hdparm tool
+if [ "${HARDDISKTYPE}" -eq 0 ]
+then
+    echo -e "${BLUE}:: ATA Secure Erase: ${CYAN}${HARDDISK}\n${OFF}"
+    hdparm --user-master u --security-set-pass NULL ${HARDDISK} | sed -n '/Issuing/s/^[ \t]*//;4,$p' && \
+    hdparm --user-master u --security-erase-enhanced NULL ${HARDDISK} | sed -n '/Issuing/s/^[ \t]*//;4,$p' && echo -e "${YELLOW}\n:: Press any key to continue...${OFF}"; read
+fi
+
+# Smartctl tool
 if [ "${HARDDISK}" == "/dev/sda" ]
 then
-    # Diagnostic
     echo -e "${BLUE}:: Smart diagnostic: ${CYAN}${HARDDISK}\n${OFF}"
     smartctl --test=short ${HARDDISK} && while [[ $(smartctl --all ${HARDDISK}) =~ 'progress' ]]; do sleep 10; done
 
-    # Statistics
     echo -e "${BLUE}\n:: Smart statistics: ${CYAN}${HARDDISK}\n${OFF}"
     smartctl --health --log=error --log=xselftest,1 ${HARDDISK}
 
-    # Status
     echo -e "${BLUE}:: Smart support: ${CYAN}${HARDDISK}\n${OFF}"
     smartctl --info ${HARDDISK} | grep 'SMART support'
 fi
