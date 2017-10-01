fd48871011357f45b8fad4f3fa062e4935f59032
kh3phr3n <rc.dev@tuxico.com>
2017-10-01 16:51:43 +0200

    Update AUR: ttf-*, premake-git, ttfautohint

--- ali/utils/aur.sh
+++ ali/utils/aur.sh
@@ -21,12 +21,12 @@ PACKAGES=(
     'i3wm/qt5-styleplugins-5.0.0-8-x86_64.pkg.tar.xz'
     # Extras fonts
     'ttf-nonfree/ttf-nonfree-1.0-1-any.pkg.tar.xz'
-    'ttf-awesome/ttf-font-awesome-4.7.0-2-any.pkg.tar.xz'
-    'ttf-iosevka/ttf-iosevka-custom-1.13.0-1-any.pkg.tar.xz'
+    'ttf-awesome/ttf-font-awesome-4.7.0-3-any.pkg.tar.xz'
+    'ttf-iosevka/ttf-iosevka-custom-1.13.3-1-any.pkg.tar.xz'
     # Extras utils
     'ttf-iosevka/otfcc-0.7.0-2-x86_64.pkg.tar.xz'
-    'ttf-iosevka/ttfautohint-1.6-1-x86_64.pkg.tar.xz'
-    'ttf-iosevka/premake-git-5.0.alpha2.r898.gad6e49c1-1-x86_64.pkg.tar.xz'
+    'ttf-iosevka/ttfautohint-1.7-1-x86_64.pkg.tar.xz'
+    'ttf-iosevka/premake-git-5.0.alpha2.r1194.g8e02b419-1-x86_64.pkg.tar.xz'
 )
 
 # Get/Install packages
