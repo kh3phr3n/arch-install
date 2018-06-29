c0e60ee3623187d35de0254602228de971dd8867
kh3phr3n <rc.dev@tuxico.com>
2018-06-29 14:59:03 +0200

    Update AUR: cower, vscode, xcursor-oxygen

--- ali/utils/aur.sh
+++ ali/utils/aur.sh
@@ -15,11 +15,11 @@ URL='https://aur.tuxico.com'
 # Packages list
 PACKAGES=(
     # Aur helper
-    'cower/cower-17-2-x86_64.pkg.tar.xz'
+    'cower/cower-18-1-x86_64.pkg.tar.xz'
     # Extras i3wm
-    'i3wm/xcursor-oxygen-5.11.1-1-any.pkg.tar.xz'
+    'i3wm/xcursor-oxygen-5.13.1-1-any.pkg.tar.xz'
     # Extras tools
-    'vscode/visual-studio-code-bin-1.23.0-1-x86_64.pkg.tar.xz'
+    'vscode/visual-studio-code-bin-1.24.1-1-x86_64.pkg.tar.xz'
     # Extras fonts
     'ttf-nonfree/ttf-nonfree-1.0-1-any.pkg.tar.xz'
     'ttf-awesome/ttf-font-awesome-4-4.7.0-5-any.pkg.tar.xz'
