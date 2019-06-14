ce87d30ecd765742f80018256db21d59a27074cc
kh3phr3n <rc.dev@tuxico.com>
2019-06-14 20:26:29 +0200

    Update AUR: auracle, ttf-iosevka-custom, rxvt-unicode-patched

--- ali/utils/aur.sh
+++ ali/utils/aur.sh
@@ -14,15 +14,15 @@ URL='https://aur.tuxico.com'
 
 # Packages list
 PACKAGES=(
-    # Aur helper
-    'cower/cower-18-1-x86_64.pkg.tar.xz'
+    # AUR helper
+    'auracle/auracle-git-r261.342de84-1-x86_64.pkg.tar.xz'
     # Extras i3wm
     'i3wm/xcursor-oxygen-5.13.1-1-any.pkg.tar.xz'
-    'i3wm/rxvt-unicode-patched-9.22-10-x86_64.pkg.tar.xz'
+    'i3wm/rxvt-unicode-patched-9.22-11-x86_64.pkg.tar.xz'
     # Extras fonts
     'ttf-nonfree/ttf-nonfree-1.0-1-any.pkg.tar.xz'
     'ttf-awesome/ttf-font-awesome-4-4.7.0-5-any.pkg.tar.xz'
-    'ttf-iosevka/ttf-iosevka-custom-2.0.2-1-any.pkg.tar.xz'
+    'ttf-iosevka/ttf-iosevka-custom-2.2.1-1-any.pkg.tar.xz'
     # Extras utils
     'ttf-iosevka/ttfautohint-1.8.3-1-x86_64.pkg.tar.xz'
     'ttf-iosevka/otfcc-0.10.3.alpha-1-x86_64.pkg.tar.xz'
