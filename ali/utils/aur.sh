bca7a17b10c8688f6d46a4e1e9a451826c942613
kh3phr3n <kh3phr3n@nixsys.fr>
2020-03-09 20:42:10 +0100

    Update AUR: auracle-git, postman-bin

--- ali/utils/aur.sh
+++ ali/utils/aur.sh
@@ -15,9 +15,9 @@ URL='https://aur.nixsys.fr'
 # Packages list
 PACKAGES=(
     # AUR helper
-    'auracle/auracle-git-r261.342de84-1-x86_64.pkg.tar.xz'
+    'auracle/auracle-git-r313.b5b41d7-1-x86_64.pkg.tar.zst'
     # Extras i3wm apps
-    'i3wm/postman-bin-7.7.3-1-x86_64.pkg.tar.xz'
+    'i3wm/postman-bin-7.19.1-1-x86_64.pkg.tar.zst'
     'i3wm/xcursor-oxygen-5.16.1-1-any.pkg.tar.xz'
     'i3wm/rxvt-unicode-patched-9.22-11-x86_64.pkg.tar.xz'
     # Extras fonts
