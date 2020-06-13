d90b825f22b7f33f180c715c1e8955413ae0aac1
kh3phr3n <kh3phr3n@nixsys.fr>
2020-06-13 10:28:35 +0200

    Update AUR: zram-generator, qt5-styleplugins

--- ali/utils/aur.sh
+++ ali/utils/aur.sh
@@ -14,14 +14,14 @@ URL='https://aur.nixsys.fr'
 
 # Packages list
 PACKAGES=(
-    # AUR helper
-    'auracle/auracle-git-r313.b5b41d7-1-x86_64.pkg.tar.zst'
-    # System apps
-    'systemd/zram-generator-0.1.2-1-x86_64.pkg.tar.zst'
     # Extras apps
-    'i3wm/postman-bin-7.19.1-1-x86_64.pkg.tar.zst'
     'i3wm/xcursor-oxygen-5.16.1-1-any.pkg.tar.xz'
+    'i3wm/postman-bin-7.19.1-1-x86_64.pkg.tar.zst'
     'i3wm/rxvt-unicode-patched-9.22-11-x86_64.pkg.tar.xz'
+    'i3wm/qt5-styleplugins-5.0.0.20170311-22-x86_64.pkg.tar.zst'
+    # System apps
+    'auracle/auracle-git-r313.b5b41d7-1-x86_64.pkg.tar.zst'
+    'systemd/zram-generator-git-0.1.2+42+g4eaf28a-1-x86_64.pkg.tar.zst'
     # Extras fonts
     'ttf-nonfree/ttf-nonfree-1.0-1-any.pkg.tar.xz'
     'ttf-pt-public/ttf-pt-public-1.0-1-any.pkg.tar.xz'
