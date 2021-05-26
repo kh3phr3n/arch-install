c956d4fd3ab08bb3e9699f5441872491860ead2b
kh3phr3n <kh3phr3n@nixsys.fr>
2021-05-26 17:38:12 +0200

    Update AUR packages to zst

--- ali/utils/aur.sh
+++ ali/utils/aur.sh
@@ -14,21 +14,24 @@ URL='https://aur.nixsys.fr'
 
 # Packages list
 PACKAGES=(
-    # Extras apps
-    'i3wm/xcursor-oxygen-5.16.1-1-any.pkg.tar.xz'
-    'i3wm/postman-bin-7.19.1-1-x86_64.pkg.tar.zst'
-    'i3wm/rxvt-unicode-patched-9.22-11-x86_64.pkg.tar.zst'
-    'i3wm/qt5-styleplugins-5.0.0.20170311-22-x86_64.pkg.tar.zst'
-    # System apps
-    'auracle/auracle-git-r313.b5b41d7-1-x86_64.pkg.tar.zst'
+    # Extras utils
+    'utils/otfcc-0.10.4-2-x86_64.pkg.tar.zst'
+    'utils/ttfautohint-1.8.3-1-x86_64.pkg.tar.zst'
+
     # Extras fonts
-    'ttf-nonfree/ttf-nonfree-1.0-1-any.pkg.tar.zst'
-    'ttf-pt-public/ttf-pt-public-1.0-1-any.pkg.tar.xz'
-    'ttf-awesome/ttf-font-awesome-4-4.7.0-5-any.pkg.tar.xz'
-    'ttf-iosevka/ttf-iosevka-custom-2.3.3-1-any.pkg.tar.zst'
-    # Extras fonts utils
-    'ttf-iosevka/ttfautohint-1.8.3-1-x86_64.pkg.tar.xz'
-    'ttf-iosevka/otfcc-0.10.3.alpha-1-x86_64.pkg.tar.xz'
+    'fonts/ttf-nonfree-1.0-1-any.pkg.tar.zst'
+    'fonts/ttf-pt-public-1.0-1-any.pkg.tar.xz'
+    'fonts/ttf-font-awesome-4-4.7.0-5-any.pkg.tar.zst'
+    'fonts/ttf-iosevka-custom-2.3.3-1-any.pkg.tar.zst'
+
+    # Extras apps
+    'apps/postman-bin-8.5.1-2-x86_64.pkg.tar.zst'
+    'apps/auracle-git-r313.b5b41d7-1-x86_64.pkg.tar.zst'
+    'apps/rxvt-unicode-patched-9.22-11-x86_64.pkg.tar.zst'
+
+    # Theme goodies
+    'goodies/xcursor-oxygen-5.16.1-1-any.pkg.tar.xz'
+    'goodies/qt5-styleplugins-5.0.0.20170311-26-x86_64.pkg.tar.zst'
 )
 
 # Get/Install packages
